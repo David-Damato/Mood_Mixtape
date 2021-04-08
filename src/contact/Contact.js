309bc75d85da918cdf503ffca58baf8186ff39f9
Aline Dubois <juillet.aline@gmail.com>
2021-04-08 21:56:34 +0200

    création de la structure de la page de contact

---
+++ src/contact/Contact.js
@@ -0,0 +1,17 @@
+import "./Contact.css";
+
+export const Contact = () => {
+    return (
+        <div className="conteneur-vertical">
+            <div className="conteneur-horizontal photos-contact">
+                <div className="conteneur-vertical photo-aline"></div>
+                <div className="conteneur-vertical photo-violaine"></div>
+                <div className="conteneur-vertical photo-pauline"></div>
+                <div className="conteneur-vertical photo-david"></div>
+                <div className="conteneur-vertical photo-tom"></div>
+            </div>
+            <div className="conteneur-horizontal titre-contact">contact</div>
+
+        </div>
+    )
+};
