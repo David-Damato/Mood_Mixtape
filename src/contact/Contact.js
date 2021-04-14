87357eb961213147bfc595d0b1559d5c476c1557
Aline Dubois <juillet.aline@gmail.com>
2021-04-14 08:37:23 +0200

    creation du hover sur les contacts

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,55 +1,10 @@
 import "./Contact.css";
 import pictoBisou from "../picto/bisou.png";
 import {PhotoContact} from "./PhotoContact";
+import {equipe} from "./equipe";
 
 export const Contact = () => {
-    const equipe = [
-        {
-            nom: "Aline DUBOIS",
-            phrase: "",
-            linkedIn: "https://www.linkedin.com/in/aline-dubois-95a0351aa/",
-            github: "https://github.com/alinedubois",
-            mail: "juillet.aline@gmail.com",
-            hauteur: 258,
-            couleur: 'orange'
-        },
-        {
-            nom: "Pauline LABBE",
-            phrase: "",
-            linkedIn: "https://www.linkedin.com/in/pauline-labb%C3%A9-360436b7/",
-            github: "https://github.com/Palabbe",
-            mail: "pauline.labbe7@gmail.com",
-            hauteur: 315,
-            couleur: 'blue'
-        },
-        {
-            nom: "Violaine ERNOTTE",
-            phrase: "",
-            linkedIn: "",
-            github: "https://github.com/vio9",
-            mail: "e.violaine@yahoo.fr",
-            hauteur: 140,
-            couleur: 'yellow'
-        },
-        {
-            nom: "David D'AMATO",
-            phrase: "",
-            linkedIn: "",
-            github: "https://github.com/David-Damato",
-            mail: "david76230@gmail.com",
-            hauteur: 530,
-            couleur: 'red'
-        },
-        {
-            nom: "Tom CARPENTIER",
-            phrase: "",
-            linkedIn: "",
-            github: "",
-            mail: "t.carpentier.19@gmail.com",
-            hauteur: 415,
-            couleur: 'green'
-        }
-    ]
+
     return (
         <div className="conteneur-vertical Contact">
             <div className="conteneur-horizontal photos-contact">
---
+++ src/contact/equipe.js
@@ -0,0 +1,47 @@
+export const equipe = [
+    {
+        nom: "Aline DUBOIS",
+        phrase: "",
+        linkedIn: "https://www.linkedin.com/in/aline-dubois-95a0351aa/",
+        github: "https://github.com/alinedubois",
+        mail: "juillet.aline@gmail.com",
+        hauteur: 258,
+        couleur: 'orange'
+    },
+    {
+        nom: "Pauline LABBE",
+        phrase: "",
+        linkedIn: "https://www.linkedin.com/in/pauline-labb%C3%A9-360436b7/",
+        github: "https://github.com/Palabbe",
+        mail: "pauline.labbe7@gmail.com",
+        hauteur: 315,
+        couleur: 'blue'
+    },
+    {
+        nom: "Violaine ERNOTTE",
+        phrase: "",
+        linkedIn: "https://www.linkedin.com/in/violaine-ernotte/",
+        github: "https://github.com/vio9",
+        mail: "e.violaine@yahoo.fr",
+        hauteur: 140,
+        couleur: 'yellow'
+    },
+    {
+        nom: "David D'AMATO",
+        phrase: "",
+        linkedIn: "https://www.linkedin.com/in/david-damato/",
+        github: "https://github.com/David-Damato",
+        mail: "david76230@gmail.com",
+        hauteur: 530,
+        couleur: 'red'
+    },
+    {
+        nom: "Tom CARPENTIER",
+        phrase: "",
+        linkedIn: "https://www.linkedin.com/in/tom-carpentier-46ab84120/",
+        github: "https://github.com/TomWebd",
+        mail: "t.carpentier.19@gmail.com",
+        hauteur: 415,
+        couleur: 'green'
+    }
+]
