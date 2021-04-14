38a44766b7a577c0ac828b2035d6656ffed8246b
[violaine ernotte] <[e.violaine@yahoo.fr]>
2021-04-14 14:25:50 +0200

    premiere version du footer avec flex et media screen terminée.

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import "./footer.css"
-import "./bisou.png"
+import React from 'react';
+import './footer.css';
+import bisou from "./bisou.png";
 
 
 const Footer = () => {
@@ -8,11 +8,16 @@ const Footer = () => {
         <footer>
             <div className="footer-container">
                 <ul className="ul-footer">
-                <li><a href="https://www.deezer.com/fr/" target="_blank">Retrouvez nos playlists sur Deezer</a></li>
-                <li><a href="https://github.com/David-Damato/Mood_Mixtape" target="_blank">projet Github</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><img src="components/Footer/bisou.png" alt="emoji bisou"></img></li>
-                <li>Made with love by : Tom Aline, Pauline, David, Violaine</li>
+                     <li>Retrouvez nos playlists sur Deezer :</li>
+                    <li><a href="https://deezer.page.link/39CMRFSHWZcPkorC9Happy">😊</a></li>
+                    <li><a href="https://deezer.page.link/awDCr7u12PyEk2ee9">😠</a></li> 
+                    <li><a href="https://deezer.page.link/5W94rtMFBAo1rpaBA">😢</a></li> 
+                    <li><a href="https://deezer.page.link/cB3gTjba8q2TL1Ak8">😍</a></li>
+                    <li><a href="#">😎</a></li>
+                    <li><a href="https://github.com/David-Damato/Mood_Mixtape" target="_blank">Projet Github</a></li>
+                     <li><a href="#">Contact</a></li>
+                     <li><a href="#"><img className="img-footer-bisou" src={bisou} alt="emoji bisou"></img></a></li>
+                    <li>Made with <strong>Love</strong> by : Tom Aline, Pauline, David, Violaine</li>
                 </ul>
             </div>
         </footer>
