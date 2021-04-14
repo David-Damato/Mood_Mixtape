56efb32085510049286fcf24c557ec05b3e75c0b
[violaine ernotte] <[e.violaine@yahoo.fr]>
2021-04-14 11:41:27 +0200

    crea branche footer, modif fichier js crea css

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import "./footer.css"
+import "./bisou.png"
+
 
 const Footer = () => {
     return (
         <footer>
             <div className="footer-container">
-                <p>Footer</p>
+                <ul className="ul-footer">
+                <li><a href="https://www.deezer.com/fr/" target="_blank">Retrouvez nos playlists sur Deezer</a></li>
+                <li><a href="https://github.com/David-Damato/Mood_Mixtape" target="_blank">projet Github</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><img src="components/Footer/bisou.png" alt="emoji bisou"></img></li>
+                <li>Made with love by : Tom Aline, Pauline, David, Violaine</li>
+                </ul>
             </div>
         </footer>
     )
