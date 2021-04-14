3d57aa1e13ec77277455b92b71c393222eda3e34
[violaine ernotte] <[e.violaine@yahoo.fr]>
2021-04-14 15:27:53 +0200

    crea css header

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -4,21 +4,25 @@ import Accueil from '../Accueil/accueil';
 import Playlist from '../Playlist/playlist';
 import Contact from '../Contact/contact';
 import Mood from '../Mood/mood';
+import './Header.css';
 import './App.css';
+
 
 class App extends Component {
     render() {
         return (
             <Router>
+                <header>
                 <div>
                     <h2>Welcome to React Router Tutorial</h2>
                     <nav className="navbar">
-                        <ul>
+                        <ul ClassName="ul-menu">
                             <li><Link to={'/'} className="nav-link">Accueil</Link></li>
                             <li><Link to={'/playlist'} className="nav-link">Playlist</Link></li>
                             <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
                         </ul>
                     </nav>
+                   
                     <hr />
                     <Switch>
                         <Route exact path='/' component={Accueil} />
@@ -27,6 +31,7 @@ class App extends Component {
                         <Route path='/contact' component={Contact} />
                     </Switch>
                 </div>
+                </header>
             </Router>
         );
     }
