182faef83d362384590c702bcc1fbae1704095d6
Palabbe <pauline.labbe7@gmail.com>
2021-04-14 10:12:11 +0200

    created component structure

--- src/components/Accueil/AccueilMoods.js
+++ src/components/Accueil/AccueilMoods.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "AccueilMoods.css";
 
 export default function MoodChoice(){
     return(
-        <div></div>
+        <div className="moodChoice">
+            <h1>Comment te sens-tu aujourd'hui ?</h1>
+            <div className="moodLinks">
+                <a href=""><img src="image/happy.png" alt="happy playlist"/></a>
+                <a href=""><img src="image/angry.png" alt="angry playlist"/></a>
+                <a href=""><img src="image/sad.png" alt="sad playlist"/></a>
+                <a href=""><img src="image/in-love.png" alt="in love playlist"/></a>
+            </div>
+        </div>
     )
 };
