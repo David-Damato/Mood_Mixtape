beb88cf59c720aa4a2a8fcb9bc900a8bbb86458b
[violaine ernotte] <[e.violaine@yahoo.fr]>
2021-04-15 18:02:07 +0200

    gestion pb de state

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -4,31 +4,29 @@ import './Navbar.css'
 import hamburger from './hamburger.png'
 
 const Navbar = () => {
-const [open, setOpen] = useState(false)
 
-const handleButtonClick = () => {
-if (!open) {
-    setOpen(true)
-} else {
-    setOpen(false)
-}
-}
- 
+const [menuOpen, setMenuOpen] = useState(false)
+
+    const menuOnClick = () => {
+        menuOpen? setMenuOpen(false) : setMenuOpen(true);
+    }
+
 
 
     return(
        <div className="menu-container">
-           <div className="menu-button" onClick={handleButtonClick }>
+           <div className="menu-button" onClick={menuOnClick}>
                 <img src={hamburger} className="burger-img" alt="burger menu"/> 
             </div>
 
-            <nav className="navbar">
-                <ul className="ul-menu">
+            <nav>
+                <ul className={menuOpen? "ul-menu-active" : "ul-menu"}>
                     <li><Link to={'/'} className="nav-link">Accueil</Link></li>
                     <li><Link to={'/playlist'} className="nav-link">Playlist</Link></li>
                     <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+                    <li><button className="closedBu"></button></li>
                 </ul>
-        </nav>
+            </nav>
         </div>
     )
 }
