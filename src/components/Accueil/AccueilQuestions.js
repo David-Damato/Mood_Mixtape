b6637c68032358cbb1bdd3fffb8c29fcbb912513
Palabbe <pauline.labbe7@gmail.com>
2021-04-16 10:17:11 +0200

    j'ai modifié le CSS pour que ce soit plus joli

--- src/components/Accueil/AccueilQuestions.js
+++ src/components/Accueil/AccueilQuestions.js
@@ -8,22 +8,11 @@ export default function Questions(){
     return(
         <div className="questionsEncadre">
             <div className="question">Qu'est-ce que Mood Mixtape ?</div>
-            <div className="reponse">Mood Mixtape est une application qui vous permet de choisir une playlist en accord avec votre humeur.</div>
-            
-            <div className="question">Vous vous êtes levé du bon pied ce matin et souriez à tout le monde ?</div>
-            <div className="reponse"> La playlist <img className="emoticone" src={happy} alt="happy"/> est faite pour vous ! </div>
-            
-            <div className="question">Le voisin a fait la fête toute la nuit, vous mettant d'humeur massacrante ?</div>
-            <div className="reponse">La playlist <img className="emoticone" src={angry} alt="angry"/> va vous défouler !</div>
-            
-            <div className="question">Vos enfants ont fait un gâteau au chocolat et ne vous ont rien laissé ?</div>
-            <div className="reponse">La playlist <img className="emoticone" src={sad} alt="sad"/> vous mettra du baume au coeur.</div>
-            
-            <div className="question">Vous êtes amoureux et cherchez une playlist pour votre date de ce soir ?</div>
-            <div className="reponse">Ne cherchez plus, la playlist <img className="emoticone" src={inLove} alt="in love"/> sera parfaite !</div>
+            <div className="reponse"><p>Créée en 2021, <em className="moodMixtape">Mood Mixtape</em> est une application qui vous permet de choisir une playlist en accord avec votre humeur.</p>
+            <p>Vous vous êtes levé du bon pied ce matin et avez envie de sourire à tout le monde ? Le voisin a fait la fête toute la nuit, vous mettant d'humeur massacrante ? Vous êtes amoureux et cherchez une playlist pour votre date de ce soir ? Que vous vous sentiez heureux, en colère, triste ou amoureux, nous avons une playslist spécialement pour vous !</p></div>
             
             <div className="question">Puis-je retrouver toutes les playlists sur une seule page ?</div>
-            <div className="reponse">Oui, bien sûr. Il suffit de vous rendre sur l'onglet « Playlists » du menu en haut de la page. Nous vous avons même préparé une playlist bonus ! </div>
+            <div className="reponse">Oui, bien sûr. Il suffit de vous rendre sur l'onglet « Playlists » du menu. Nous vous avons même préparé une playlist bonus ! </div>
             
         </div>
     )
