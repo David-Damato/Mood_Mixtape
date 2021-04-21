9d189ba7d1f1b3eebbf880ae6d197b0f9a56341c
Palabbe <pauline.labbe7@gmail.com>
2021-04-21 14:35:36 +0200

    ajustement css encadré questions

--- src/components/Accueil/AccueilQuestions.js
+++ src/components/Accueil/AccueilQuestions.js
@@ -6,7 +6,7 @@ export default function Questions(){
     return(
         <div className="questionsEncadre">
             <div onClick={() => setReponseOneAppears((prevState) => !prevState)}
-            className="question">Qu'est-ce que <em className="moodMixtape">Mood Mixtape ?</em></div>
+            className="question"><span>Qu'est-ce que <em className="moodMixtape">Mood Mixtape ?</em> </span></div>
             <div className={reponseOneAppears ? "reponse-appears" : "reponse-hidden"}><p>Créée en 2021, <em className="moodMixtape">Mood Mixtape</em> est une application qui vous permet de choisir une playlist en accord avec votre humeur.</p>
             <p>Vous vous êtes levé du bon pied ce matin et avez envie de sourire à tout le monde ? Le voisin a fait la fête toute la nuit, vous mettant d'humeur massacrante ? Vous êtes amoureux et cherchez une playlist pour votre date de ce soir ? Que vous vous sentiez heureux, en colère, triste ou amoureux, nous avons une playslist spécialement pour vous !</p></div>
             
