3246accc5e17ed6f1f368e23c4e2816fc8e568d0
Palabbe <pauline.labbe7@gmail.com>
2021-04-21 10:01:03 +0200

    j'ai remplacé les pixels par des pourcentages dans le fichier CSS + supprimé les lignes de code inutiles dans AccueilQuestions.js

--- src/components/Accueil/AccueilQuestions.js
+++ src/components/Accueil/AccueilQuestions.js
@@ -1,8 +1,4 @@
 import React from "react";
-import happy from "./images/happy.png";
-import angry from "./images/angry.png";
-import sad from "./images/sad.png";
-import inLove from "./images/in-love.png";
 
 export default function Questions(){
     return(
