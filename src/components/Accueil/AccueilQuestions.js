b181385243dd652552868a0c6dee7294dbdee239
Palabbe <pauline.labbe7@gmail.com>
2021-04-21 11:29:12 +0200

    animations encadré moods ok

--- src/components/Accueil/AccueilQuestions.js
+++ src/components/Accueil/AccueilQuestions.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Questions(){
     return(
         <div className="questionsEncadre">
-            <div className="question">Qu'est-ce que Mood Mixtape ?</div>
+            <div className="question">Qu'est-ce que <em className="moodMixtape">Mood Mixtape ?</em></div>
             <div className="reponse"><p>Créée en 2021, <em className="moodMixtape">Mood Mixtape</em> est une application qui vous permet de choisir une playlist en accord avec votre humeur.</p>
             <p>Vous vous êtes levé du bon pied ce matin et avez envie de sourire à tout le monde ? Le voisin a fait la fête toute la nuit, vous mettant d'humeur massacrante ? Vous êtes amoureux et cherchez une playlist pour votre date de ce soir ? Que vous vous sentiez heureux, en colère, triste ou amoureux, nous avons une playslist spécialement pour vous !</p></div>
             
