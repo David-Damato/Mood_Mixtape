510df8d9ab404afb9a83687e16e036d63c5c14c3
Aline Dubois <juillet.aline@gmail.com>
2021-04-25 16:24:16 +0200

    réorganisation en composants de la page playlist

---
+++ src/components/Playlist/DetailPlaylist.js
@@ -0,0 +1,54 @@
+import "./DetailPlaylist.css";
+
+export const DetailPlaylist = ({type, playlist}) => {
+    return (
+        <div className={`playlist playlist-${type.toLowerCase()}`}>
+            <table>
+                <thead className="theadPlaylist">
+                <h2>Playlist {type}</h2>
+                <div id={`detail${type}`} className={`detail${type}`}>
+                </div>
+                </thead>
+                <tbody className="tbodyPlaylist">
+                <tr>
+                    <th><h3>Titre</h3></th>
+                    <th><h3>Artiste</h3></th>
+                    <th><h3>Album</h3></th>
+                    <th><h3>Détails</h3></th>
+                </tr>
+                {playlist.map(track =>
+                    <>
+                        <tr className="trPlaylist">
+                            <td className={`tdPlaylist td${type}`}>
+                                {track.title}
+                            </td>
+                            <td className={`tdPlaylist td${type}`}>
+                                {track.artist.name}
+                            </td>
+                            <td className={`tdPlaylist td${type}`}>
+                                {track.album.title}
+                            </td>
+                            <td className="">
+                                <button className={`buttonDetail button${type}`} key={track}
+                                        onClick={() => document.getElementById("detail"+type).innerHTML = `
+                                        <img style="border-radius: 10px" src='${track.album.cover_medium}' alt='Image_Album'>
+                                        <h3>Extrait:</h3>
+                                        <audio controls name="media"><source src="${track.preview}"></audio>
+                                        <h3>Titre:</h3>
+                                        ${track.title}
+                                        <h3>Artiste:</h3>
+                                        ${track.artist.name}
+                                        <h3>Album:</h3>
+                                        ${track.album.title}
+                                        `}>Afficher
+                                </button>
+                            </td>
+                        </tr>
+                        <tr className="espaceTr"></tr>
+                    </>
+                )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
