1572e428a992e0bfe0c9528d641e1d5f2bc9801d
Palabbe <pauline.labbe7@gmail.com>
2021-04-30 16:53:59 +0200

    nouvelle proposition bouton caché avec sparkles

--- src/components/Accueil/AccueilMoods.js
+++ src/components/Accueil/AccueilMoods.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-export default function MoodChoice() {
+import Sparkle from "react-sparkle";
+
+export default function MoodChoice() {    
     return (
         <div className="moodChoice">
             <h1>Comment vous sentez-vous aujourd'hui ?</h1>
@@ -20,8 +22,43 @@ export default function MoodChoice() {
                     alt="in love playlist" /></Link>
 
             </div>
-            <div className="boutonBonusContainer">
-            <Link to="/MoodBonus"><button className="boutonBonus">Il y a une surprise par ici !</button></Link>
+            <div className="buttonBonusContainer">
+            <Link to="/MoodBonus">
+                <button className="buttonBonusAccueil">Il y a une surprise par ici ! (っ▀¯▀)つ
+                <Sparkle
+                color={'white'}
+                // The number of sparkles to render. A large number could slow
+                // down the page.
+                count={20}
+                
+                // The minimum and maximum diameter of sparkles, in pixels.
+                minSize={8}
+                maxSize={15}
+                
+                // The number of pixels the sparkles should extend beyond the
+                // bounds of the parent element.
+                overflowPx={20}
+                
+                // How quickly sparkles disappear; in other words, how quickly
+                // new sparkles are created. Should be between 0 and 1000,
+                // with 0 never fading sparkles out and 1000 immediately
+                // removing sparkles. Most meaningful speeds are between
+                // 0 and 150.
+                fadeOutSpeed={20}
+                
+                // Whether we should create an entirely new sparkle when one
+                // fades out. If false, we'll just reset the opacity, keeping
+                // all other attributes of the sparkle the same.
+                newSparkleOnFadeOut={true}
+                
+                // Whether sparkles should have a "flickering" effect.
+                flicker={false}
+                
+                // How quickly the "flickering" should happen.
+                // One of: 'slowest', 'slower', 'slow', 'normal', 'fast', 'faster', 'fastest'
+                flickerSpeed={'slower'}/>
+                </button>
+            </Link>
             </div>
         </div>
     )
